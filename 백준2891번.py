b5994b1f42f9e054758dbcda8bec6f3abe9d2ce3
leesu991012 <leesu1012@gmail.com>
2021-03-09 04:38:57 +0900

    백준 2891번 (정답)
    백준 2891번 "카약과 광풍"

--- 백준2891번.py
+++ 백준2891번.py
@@ -8,8 +8,23 @@ for i in damaged:
 for i in spare:
     li[i - 1] += 1
 
-print(li)
+# print(li)
 
+if li[0] == 0 and li[1] == 2:
+    li[0], li[1] = 1, 1
+if li[n - 1] == 0 and li[n - 2] == 2:
+    li[n - 1], li[n - 2] = 1, 1
 
+# print(li)
 
+for i in range(1, n - 1):
+    if (li[i] == 0):
+        if (li[i - 1] == 2):
+            li[i - 1] -= 1
+            li[i] += 1
+        elif (li[i + 1] == 2):
+            li[i + 1] -= 1
+            li[i] += 1
+
+# print(li)
 print(li.count(0))
