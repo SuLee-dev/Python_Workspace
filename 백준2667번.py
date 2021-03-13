e2881c2575568d44c67965be824a5d30fbaa5e2c
leesu991012 <leesu1012@gmail.com>
2021-03-13 21:51:43 +0900

    백준 2667번 (정답)
    백준 2667번 "단지번호붙이기"

--- 백준2667번.py
+++ 백준2667번.py
@@ -1,51 +1,40 @@
 from collections import deque
+import sys
 
-# 리스트 내의 "1"이 나오는 첫 위치를 반환하는 함수
-def findStart(li, num):
-    for i in range(num):
-        if (li[i].count("1") > 0):
-            x, y = i, li[i].index("1")
-            return x, y
-    return False
-
-n = int(input())
+count = []
+n = int(sys.stdin.readline())
 
 s = []
 for _ in range(n):
-    s.append(list(input()))
+    s.append(list(sys.stdin.readline()))
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-cnt = 0 # 단지 수
+def bfs(a, b):
+    dq = deque([[a, b]])
+    cnt = 1
+    s[a][b] = "0"
+    while dq:
+        v = dq.popleft()
+        x, y = v[0], v[1]
 
-# 리스트 안의 "1"을 해당되는 단지 수로 변환
-while True:
-    if (findStart(s, n) != False):
-        a, b = findStart(s, n)
-        cnt += 1
-        dq = deque([[a, b]])
+        for i in range(4):
+            x1 = x + dx[i]
+            y1 = y + dy[i]
+            if 0 <= x1 < n and 0 <= y1 < n and s[x1][y1] == "1":
+                s[x1][y1] = "0"
+                dq.append([x1, y1])
+                cnt += 1
+    count.append(cnt)
 
-        while dq:
-            v = dq.popleft()
-            x, y = v[0], v[1]
-
-            for i in range(4):
-                x1 = x + dx[i]
-                y1 = y + dy[i]
-                if 0 <= x1 < n and 0 <= y1 < n and s[x1][y1] == "1":
-                    s[x1][y1] = cnt
-                    dq.append([x1, y1])
-    else:
-        break
-
-# 리스트를 순회하면서 각 단지 내의 집 개수 세기
-ans = [0] * (cnt + 1)
 for i in range(n):
     for j in range(n):
-        if (type(s[i][j]) == int):
-            ans[s[i][j]] += 1
+        if s[i][j] == "1":
+            bfs(i, j)
 
-print(cnt)
-for i in range(1, cnt + 1):
-    print(ans[i])
+count.sort()
+
+print(len(count))
+for i in count:
+    print(i)
