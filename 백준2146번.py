d36590856e13bedefd24f9466f2652bdeb2f6311
leesu991012 <leesu1012@gmail.com>
2021-03-17 20:22:10 +0900

    백준 2146번 (정답)
    백준 2146번 "다리 만들기"

--- 백준2146번.py
+++ 백준2146번.py
@@ -12,9 +12,10 @@ for _ in range(n):
     s.append(list(map(int, input().split())))
 
 
-count = -1
+count = 0
 def bfs(a, b):
     global count
+    count -= 1
     queue = deque([[a, b]])
     s[a][b] = count
 
@@ -28,9 +29,40 @@ def bfs(a, b):
                 if s[x1][y1] == 1:
                     queue.append([x1, y1])
                     s[x1][y1] = count
-    count -= 1
 
 for i in range(n):
     for j in range(n):
         if s[i][j] == 1:
             bfs(i, j)
+
+ans = 10000
+def bfs2(z):
+    global ans
+    queue = deque()
+    dist = [[-1] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if s[i][j] == z:
+                dist[i][j] = 0
+                queue.append([i, j])
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            x1 = x + dx[i]
+            y1 = y + dy[i]
+
+            if x1 < 0 or x1 >= n or y1 < 0 or y1 >= n:
+                continue
+            if s[x1][y1] < 0 and s[x1][y1] != z:
+                ans = min(ans, dist[x][y])
+                return
+            if s[x1][y1] == 0 and dist[x1][y1] == -1:
+                dist[x1][y1] = dist[x][y] + 1
+                queue.append([x1, y1])
+
+for i in range(count, 0):
+    bfs2(i)
+
+print(ans, count)
