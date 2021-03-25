07c6152d7ec16833f8f06aa0432f314f33b4f3d8
leesu991012 <leesu1012@gmail.com>
2021-03-26 00:44:14 +0900

    9강 마무리
    5, 6번 문제

--- 9강.py
+++ 9강.py
@@ -95,7 +95,7 @@ class MyStack:
         """
         Returns whether the stack is empty.
         """
-        return len(q) == 0
+        return len(self.q) == 0
 
 
 # Your MyStack object will be instantiated and called as such:
@@ -104,3 +104,100 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+
+# 리트코드 232번
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.input.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.input == [] and self.output == []
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+
+
+# 리트코드 622번
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = [None] * k
+        self.maxlen = k
+        self.p1 = 0
+        self.p2 = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.q[self.p2] is None:
+            self.q[self.p2] = value
+            self.p2 = (self.p2 + 1) % self.maxlen
+            return True
+        else:
+            return False
+
+    def deQueue(self) -> bool:
+        if self.q[self.p1] is None:
+            return False
+        else:
+            self.q[self.p1] = None
+            self.p1 = (self.p1 + 1) % self.maxlen
+            return True
+
+    def Front(self) -> int:
+        return -1 if self.q[self.p1] is None else self.q[self.p1]
+
+    def Rear(self) -> int:
+        return -1 if  self.q[self.p2 - 1] is None else self.q[self.p2 - 1]
+
+    def isEmpty(self) -> bool:
+        return self.p1 == self.p2 and self.q[self.p1] is None
+
+    def isFull(self) -> bool:
+        return self.p1 == self.p2 and self.q[self.p1] is not None
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
