7236a09353cd33d64e004fa1f403974a456a6105
leesu991012 <leesu1012@gmail.com>
2021-03-26 23:55:20 +0900

    10강 데크, 우선순위 큐
    1, 2번 문제

---
+++ 10강.py
@@ -0,0 +1,122 @@
+# 리트코드 641번
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the deque to be k.
+        """
+        self.head, self.tail = ListNode(None), ListNode(None)
+        self.k, self.len = k, 0
+        self.head.right, self.tail.left = self.tail, self.head
+
+    def _add(self, node: ListNode, new: ListNode):
+        n = node.right
+        node.right = new
+        new.left, new.right = node, n
+        n.left = new
+
+    def _del(self, node: ListNode):
+        n = node.right.right
+        node.right = n
+        n.left = node
+
+    def insertFront(self, value: int) -> bool:
+        """
+        Adds an item at the front of Deque. Return true if the operation is successful.
+        """
+        if self.len == self.k:
+            return False
+        else:
+            self.len += 1
+            self._add(self.head, ListNode(value))
+            return True
+
+    def insertLast(self, value: int) -> bool:
+        """
+        Adds an item at the rear of Deque. Return true if the operation is successful.
+        """
+        if self.len == self.k:
+            return False
+        else:
+            self.len += 1
+            self._add(self.tail.left, ListNode(value))
+            return True
+
+    def deleteFront(self) -> bool:
+        """
+        Deletes an item from the front of Deque. Return true if the operation is successful.
+        """
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self._del(self.head)
+        return True
+
+    def deleteLast(self) -> bool:
+        """
+        Deletes an item from the rear of Deque. Return true if the operation is successful.
+        """
+        if self.len == 0:
+            return False
+        self.len -= 1
+        self._del(self.tail.left.left)
+        return True
+
+    def getFront(self) -> int:
+        """
+        Get the front item from the deque.
+        """
+        return self.head.right.val if self.len else -1
+
+    def getRear(self) -> int:
+        """
+        Get the last item from the deque.
+        """
+        return self.tail.left.val if self.len else -1
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular deque is empty or not.
+        """
+        return self.len == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular deque is full or not.
+        """
+        return self.len == self.k
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
+
+
+# 리트코드 23번
+
+def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    root = result = ListNode(None)
+    heap = []
+
+    for i in range(len(lists)):
+        if lists[i]:
+            heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+    while heap:
+        node = heapq.heappop(heap)
+        idx = node[1]
+        result.next = node[2]
+
+        result = result.next
+        if result.next:
+            heapq.heappush(heap, (result.next.val, idx, result.next))
+
+    return root.next
