cc8ae03d076f994e26ecae0f30adf75da4e3e3a9
leesu991012 <leesu1012@gmail.com>
2021-04-03 03:20:03 +0900

    14강 (진행중)
    4~7번 문제

--- 14강.py
+++ 14강.py
@@ -73,3 +73,120 @@ class Solution:
         
         dfs(root)
         return self.result
+
+
+# 리트코드 226번
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+
+                queue.append(node.left)
+                queue.append(node.right)
+        
+        return root
+
+# 재귀
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
+
+
+# 리트코드 617번
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
+
+            return node
+        else:
+            return root1 or root2
+
+
+# 리트코드 297번
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        result = ['#']
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+                result.append(str(node.val))
+            else:
+                result.append('#')
+
+        return ' '.join(result)
+
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == '# #':
+            return None
+
+        nodes = data.split()
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+
+        index = 2
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1           
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+# 리트코드 110번
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def check(root):
+            if not root:
+                return 0
+            
+            left = check(root.left)
+            right = check(root.right)
+
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            
+            return max(left, right) + 1
+        
+        return check(root) != -1
