66b93c5d1190d8f305be3a2f5d21dd137248770d
leesu991012 <leesu1012@gmail.com>
2021-04-12 23:49:09 +0900

    19강 (진행중)
    3번 문제

--- 19강.py
+++ 19강.py
@@ -13,3 +13,19 @@ def singleNumber(self, nums: List[int]) -> int:
 def hammingDistance(self, x: int, y: int) -> int:
 
     return bin(x ^ y).count('1')
+
+
+# 리트코드 371번
+
+def getSum(self, a: int, b: int) -> int:
+
+    MASK = 0xFFFFFFFF
+    INT_MAX = 0x7FFFFFFF
+
+    while b != 0:
+        a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
+
+    if a > INT_MAX:
+        a = ~(a ^ MASK)
+
+    return a
