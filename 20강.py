2a9ebaa3a6983ea1c50975f55171ce41522f2734
leesu991012 <leesu1012@gmail.com>
2021-04-14 22:29:30 +0900

    20강 마무리
    4, 5번 문제

--- 20강.py
+++ 20강.py
@@ -16,3 +16,44 @@ def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
                 results.append(nums[queue[0]])
             
         return results
+
+
+# 리트코드 76번
+
+def minWindow(self, s: str, t: str) -> str:
+    need = collections.Counter(t)
+    missing = len(t)
+    left = start = end = 0
+
+    for char, right in enumerate(s, 1):
+        missing -= need[char] > 0
+        need[char] -= 1
+
+        if missing == 0:
+            while left < right and need[s[left]] < 0:
+                need[s[left]] += 1
+                left += 1
+            
+            if not end or right - left <= end - start:
+                start, end = left, right
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+    return s[start:end]
+
+
+# 리트코드 424번
+
+def characterReplacement(self, s: str, k: int) -> int:
+    counts = collections.Counter()
+    left = right = 0
+
+    for right in range(1, len(s) + 1):
+        counts[s[right - 1]] += 1
+        max_char_n = counts.most_common(1)[0][1]
+
+        if right - left - max_char_n > k:
+            counts[s[left]] -= 1
+            left += 1
+
+    return rught - left
